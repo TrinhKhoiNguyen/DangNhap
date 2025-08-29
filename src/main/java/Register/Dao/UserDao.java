8d6a49b0af45f40e84e89d45a9ef7346ac51e492
Nguyencte20 <23162068@student.hcmute.edu.vn>
2025-08-29 18:41:08 +0700

    Delete src directory

--- src/main/java/Register/Dao/UserDao.java
+++
@@ -1,7 +0,0 @@
-package Register.Dao;
-
-import Register.Model.User;
-
-public interface UserDao {
-    User findByUserName(String username);
-}
--- src/main/java/Register/Service/UserService.java
+++
@@ -1,8 +0,0 @@
-package Register.Service;
-
-import Register.Model.User;
-
-public interface UserService {
-    User login(String username, String password);
-    User findByUserName(String username);
-}
